86e815a05c02eccca8b3de1d53786a9d1a517f16
Dawid B <l33t.c4ch3@gmail.com>
2022-11-24 09:01:29 +0100

    Added Night Mode support

--- app/src/main/java/com/example/reactiontime/MainActivity.java
+++ app/src/main/java/com/example/reactiontime/MainActivity.java
@@ -1,19 +1,53 @@
 package com.example.reactiontime;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //initialize buttons
+        SwitchCompat sch = findViewById(R.id.sch_mode);
         Button btnSolo = findViewById(R.id.btnSolo);
+
+        //Save user color preference
+        SharedPreferences preferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
+        final SharedPreferences.Editor editor = preferences.edit();
+        final boolean darkModeEnabled = preferences.getBoolean("darkModeEnabled", false);
+        if(darkModeEnabled) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            sch.setText(getString(R.string.night_mode_on));
+        }
+        else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            sch.setText(getString(R.string.night_mode_off));
+        }
+
+        //Switch to night mode based on the toggle position
+        sch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                sch.setText(getString(R.string.night_mode_on));
+                editor.putBoolean("darkModeEnabled", true);
+                editor.apply();
+
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                sch.setText(getString(R.string.night_mode_off));
+                editor.putBoolean("darkModeEnabled", false);
+                editor.apply();
+            }
+        });
+
         //set on click listeners
         btnSolo.setOnClickListener(view -> SoloActivity());
     }
--- app/src/main/java/com/example/reactiontime/SoloActivity.java
+++ app/src/main/java/com/example/reactiontime/SoloActivity.java
@@ -6,7 +6,6 @@ import java.util.Random;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
 import android.widget.TextView;
 import org.apache.commons.lang3.time.StopWatch;
 
@@ -34,26 +33,24 @@ public class SoloActivity extends AppCompatActivity {
             //Handle the setup
             case 1:
                 textSolo.setText(R.string.tap_to_start);
-                textSolo.setTextColor(getResources().getColor(R.color.text_neutral_light));
-                layout.setBackgroundColor(getResources().getColor(R.color.bg_neutral_light));
+                textSolo.setTextColor(getResources().getColor(R.color.text_neutral));
+                layout.setBackgroundColor(getResources().getColor(R.color.bg_neutral));
                 gameState = 2;
                 break;
             //Handle the game
             case 2:
                 textSolo.setText(R.string.tap_when_green);
-                textSolo.setTextColor(getResources().getColor(R.color.text_neutral_light));
+                textSolo.setTextColor(getResources().getColor(R.color.text_neutral));
                 //Randomizing the delay
                 Random rand = new Random();
                 delay = rand.nextInt(5000);
                 delay += 1000;
                 //Change screen color after random delay
-                Runnable delayedRunnable = new Runnable() {
-                    @Override
-                    public void run() {
-                        layout.setBackgroundColor(getResources().getColor(R.color.bg_tap_now));
-                        textSolo.setText(R.string.tap_now);
-                        stopwatch.start();
-                    }
+                Runnable delayedRunnable = () -> {
+                    textSolo.setTextColor(getResources().getColor(R.color.text_success));
+                    layout.setBackgroundColor(getResources().getColor(R.color.bg_tap_now));
+                    textSolo.setText(R.string.tap_now);
+                    stopwatch.start();
                 };
                 delayHandler.postDelayed(delayedRunnable, delay);
                 gameState = 3;
@@ -90,8 +87,8 @@ public class SoloActivity extends AppCompatActivity {
         TextView textSolo = findViewById(R.id.textSolo);
         ConstraintLayout layout = findViewById(R.id.layout_solo);
         textSolo.setText(R.string.tap_to_start);
-        textSolo.setTextColor(getResources().getColor(R.color.text_neutral_light));
-        layout.setBackgroundColor(getResources().getColor(R.color.bg_neutral_light));
+        textSolo.setTextColor(getResources().getColor(R.color.text_neutral));
+        layout.setBackgroundColor(getResources().getColor(R.color.bg_neutral));
         gameState = 2;
     }
 
